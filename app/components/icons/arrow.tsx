0e54a0959aa4a22dbe954bb25e53743fcde0b5a2
leonardogarreto <leonardogarreto@gmail.com>
2024-11-01 17:06:53 -0300

    fix(icons): svg props errors

--- app/components/icons/arrow.tsx
+++ app/components/icons/arrow.tsx
@@ -7,9 +7,6 @@ export const Arrow = () => {
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
       className="icon icon-tabler icons-tabler-outline icon-tabler-arrow-left"
     >
       <path stroke="none" d="M0 0h24v24H0z" fill="none" />
