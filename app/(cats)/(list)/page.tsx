f761d8284c78147f68b5525b43f93853ae549817
leonardogarreto <leonardogarreto@gmail.com>
2024-11-04 07:25:28 -0300

    fix(cats): wrap use search params with suspense

--- app/(cats)/(list)/page.tsx
+++ app/(cats)/(list)/page.tsx
@@ -1,8 +1,9 @@
 "use client";
 
-import { useState } from "react";
+import { Suspense, useState } from "react";
 import { Filters } from "./filters";
 import { CatsList } from "./list";
+import { LoadingIcon } from "@/app/components/loading-icon";
 
 export default function Home() {
   const [filterId, setFilterId] = useState<number | undefined>();
@@ -10,7 +11,9 @@ export default function Home() {
   return (
     <div className="flex flex-col gap-2 py-4 mt-8 mx-auto w-full">
       <div className="flex flex-col px-8 gap-4 md:px-16 lg:px-24">
-        <Filters setFilterId={setFilterId} filterId={filterId} />
+        <Suspense fallback={<LoadingIcon />}>
+          <Filters setFilterId={setFilterId} filterId={filterId} />
+        </Suspense>
         <CatsList filterId={filterId} />
       </div>
     </div>
